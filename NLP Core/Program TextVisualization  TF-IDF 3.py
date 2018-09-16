c1bd1f04e1474fb26421864bf15bbf3165ac4637
Rishav <rishav.jnit@gmail.com>
2018-09-16 17:34:56 +0530

    Building Character N-Gram MMOdel

--- NLP Core/Program TextVisualization  TF-IDF 3.py
+++ NLP Core/Program TextVisualization  TF-IDF 3.py
@@ -1,11 +1,6 @@
-# Natural Language Processing using NLTK
-
-# Install NLTK - pip install nltk
-
-# Tokenization of paragraphs/sentences
 import nltk
-import re
 import heapq
+import re
 import numpy as np
 
 paragraph = """Thank you all so very much. Thank you to the Academy. 
@@ -37,62 +32,56 @@ paragraph = """Thank you all so very much. Thank you to the Academy.
                out by the politics of greed. I thank you all for this 
                amazing award tonight. Let us not take this planet for 
                granted. I do not take tonight for granted. Thank you so very much."""
-               
-               
-# Tokenize sentences
+
 dataset = nltk.sent_tokenize(paragraph)
+
 for i in range(len(dataset)):
-    dataset[i] = dataset[i].lower()
-    dataset[i] = re.sub(r'\W',' ',dataset[i])
-    dataset[i] = re.sub(r'\s+',' ',dataset[i])
+     dataset[i] = dataset[i].lower()
+     dataset[i] = re.sub(r'\W',' ',dataset[i])
+     dataset[i] = re.sub(r'\s+',' ',dataset[i])
 
-
-# Creating word histogram
+#Create the histogram
 word2count = {}
 for data in dataset:
-    words = nltk.word_tokenize(data)
-    for word in words:
-        if word not in word2count.keys():
-            word2count[word] = 1
-        else:
-            word2count[word] += 1
-            
-# Selecting best 100 features
+     words = nltk.word_tokenize(data)
+     for word in words:
+          if word not in word2count.keys():
+               word2count[word] = 1
+          else:
+               word2count[word] += 1
+
 freq_words = heapq.nlargest(100,word2count,key=word2count.get)
 
+#IDF Matrix
 
-# IDF Dictionary
 word_idfs = {}
 for word in freq_words:
-    doc_count = 0
-    for data in dataset:
-        if word in nltk.word_tokenize(data):
-            doc_count += 1
-    word_idfs[word] = np.log(len(dataset)/(1+doc_count))
-    
-# TF Matrix
+     doc_count = 0
+     for  data in dataset:
+          if word in nltk.word_tokenize(data):
+               doc_count += 1
+     word_idfs[word] = np.log((len(dataset)/doc_count) + 1)
+#TF Matrix    
 tf_matrix = {}
 for word in freq_words:
-    doc_tf = []
-    for data in dataset:
-        frequency = 0
-        for w in nltk.word_tokenize(data):
-            if word == w:
-                frequency += 1
-        tf_word = frequency/len(nltk.word_tokenize(data))
-        doc_tf.append(tf_word)
-    tf_matrix[word] = doc_tf
-    
-# Creating the Tf-Idf Model
-tfidf_matrix = []
-for word in tf_matrix.keys():
-    tfidf = []
-    for value in tf_matrix[word]:
-        score = value * word_idfs[word]
-        tfidf.append(score)
-    tfidf_matrix.append(tfidf)   
-    
-# Finishing the Tf-Tdf model
-X = np.asarray(tfidf_matrix)
+     doc_tf = []
+     for data in dataset:
+          frequency = 0
+          for w in nltk.word_tokenize(data):
+               if w == word:
+                    frequency += 1
+          tf_word = frequency/len(nltk.word_tokenize(data))
+          doc_tf.append(tf_word)
+     tf_matrix[word] = doc_tf;
 
+#TF_IDF CalCulation
+tfidf_matrix =[]
+for word in tf_matrix.keys():
+     tfidf = []
+     for value in tf_matrix[word]:
+          score = value * word_idfs[word]
+          tfidf.append(score)
+     tfidf_matrix.append(tfidf)
+
+X = np.asarray(tfidf_matrix)
 X = np.transpose(X)
